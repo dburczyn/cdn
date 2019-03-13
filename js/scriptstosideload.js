2e2bc4420071abc78ad415acd9d36e7bf965cdae
dburczyn <47969136+dburczyn@users.noreply.github.com>
2019-03-13 16:00:20 +0100

    upd

--- js/scriptstosideload.js
+++ js/scriptstosideload.js
@@ -20,11 +20,11 @@ jQuery_1_12_4.ajax({
                 url: 'http://cdn.jsdelivr.net/gh/dburczyn/cdn/js/' + f.name,
                 dataType: 'json',
 
-                         success: function (response) {
-                        jQuery_1_12_4.each(response, function (e, r) {
-                       var tile =     '<div class="col-md-4 cms-boxes-outer">        <div class="cms-boxes-items cms-features">          <div class="boxes-align">            <div class="small-box">              <i class="fa fa-4x fa-laptop">&nbsp;</i>              <h2>'+e+'</h2>              <p>'+JSON.stringify(r)+'</p>            </div>          </div>        </div>      </div>';
-            jQuery_1_12_4(tile).appendTo("#viewPanelTile");
-                   })}
+                     success: function (response) {
+
+                       var tile =     '<div class="col-md-4 cms-boxes-outer">        <div class="cms-boxes-items cms-features">          <div class="boxes-align">            <div class="small-box">              <i class="fa fa-4x fa-laptop">&nbsp;</i>              <h2>'+f.name+'</h2>              <p>'+JSON.stringify(response)+'</p>            </div>          </div>        </div>      </div>';
+                         jQuery_1_12_4(tile).appendTo("#foo");
+                  }
 
                 });
 
