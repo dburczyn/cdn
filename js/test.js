e8697a338c0a81241b1c72d0f0c6772509bd1e3a
dburczyn <47969136+dburczyn@users.noreply.github.com>
2019-03-13 13:11:44 +0100

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,1 +1,55 @@
 
+{
+    "444": {
+        "name": "Append String Microservice",
+        "Microservice_ID": "0b3f3e95-a35f-4727-9f29-c21075c3a123",
+        "Microservice_Operation": "default",
+        "x": 1060,
+        "y": 520,
+        "input": {
+            "input": {
+                "value": "hello my new microservice!!"
+            }
+        },
+        "wires": "98da5c37-6aef-4da7-847b-e788720aeeac"
+    },
+    "555": {
+        "name": "REST Microservice",
+        "Microservice_ID": "98da5c37-6aef-4da7-847b-e788720aeeac",
+        "Microservice_Operation": "default",
+        "x": 700,
+        "y": 300,
+        "input": {
+            "input": {
+                "value": "hello my new microservice!!"
+            }
+        },
+        "wires": ""
+    },
+    "666":{
+        "name": "Append String Microservice",
+        "Microservice_ID": "0b3f3e95-a35f-4727-9f29-c21075c3a123",
+        "Microservice_Operation": "default",
+        "x": 1060,
+        "y": 520,
+        "input": {
+            "input": {
+                "value": "hello my new microservice!!"
+            }
+        },
+        "wires": "98da5c37-6aef-4da7-847b-e788720aeeac"
+    } ,
+    "777": {
+        "name": "Append String Microservice",
+        "Microservice_ID": "0b3f3e95-a35f-4727-9f29-c21075c3a123",
+        "Microservice_Operation": "default",
+        "x": 1060,
+        "y": 520,
+        "input": {
+            "input": {
+                "value": "hello my new microservice!!"
+            }
+        },
+        "wires": "98da5c37-6aef-4da7-847b-e788720aeeac"
+    }
+}
