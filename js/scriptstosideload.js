d6423a874af3fd92847c1d960d763bf3ed741a71
dburczyn <47969136+dburczyn@users.noreply.github.com>
2019-03-13 15:47:51 +0100

    change tile

--- js/scriptstosideload.js
+++ js/scriptstosideload.js
@@ -22,7 +22,7 @@ jQuery_1_12_4.ajax({
 
                          success: function (response) {
                         jQuery_1_12_4.each(response, function (e, r) {
-                       var tile = '    <div class="container">        <section class="cms-boxes">            <div class="container-fluid">                <div class="row">                    <div class="col-md-4 cms-boxes-outer">                        <div class="cms-boxes-items cms-features">                            <div class="boxes-align">                                <div class="small-box">                                    <i class="fa fa-4x fa-laptop">&nbsp;</i>                                    <h2>' + e + '</h2>                                    <p>'   + JSON.stringify(r) +   '</p>                                </div>                            </div>                        </div>                    </div>                </div>            </div>        </section>    </div>'
+                       var tile =     '<div class="col-md-4 cms-boxes-outer">        <div class="cms-boxes-items cms-features">          <div class="boxes-align">            <div class="small-box">              <i class="fa fa-4x fa-laptop">&nbsp;</i>              <h2>'+e+'</h2>              <p>'+JSON.stringify(r)+'</p>            </div>          </div>        </div>      </div>';
             jQuery_1_12_4(tile).appendTo("#viewPanelTile");
                    })}
 
