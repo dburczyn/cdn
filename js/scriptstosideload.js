74659c4d66f78260ae236eb35141aa56712176e5
dburczyn <47969136+dburczyn@users.noreply.github.com>
2019-03-13 15:16:02 +0100

    jquery noconflict

--- js/scriptstosideload.js
+++ js/scriptstosideload.js
@@ -1,5 +1,5 @@
 
-$(function () {
+jQuery_1_12_4(function () {
     $('#tile_form').on('submit', function (e) {
         e.preventDefault();
 
@@ -7,7 +7,7 @@ $(function () {
 
 
 
-$.ajax({
+jQuery_1_12_4.ajax({
     url:  ' https://api.github.com/repos/dburczyn/cdn/contents/js/',
     dataType: 'json',
     success: function(results)   {
@@ -15,15 +15,15 @@ $.ajax({
 
 
             var list = '<p>'+JSON.stringify(f.name)+'</p>';
-            $(list).appendTo("#viewPanelTile");
-            $.ajax({
+            jQuery_1_12_4(list).appendTo("#viewPanelTile");
+            jQuery_1_12_4.ajax({
                 url: 'http://cdn.jsdelivr.net/gh/dburczyn/cdn/js/' + f.name,
                 dataType: 'json',
 
                          success: function (response) {
-                        $.each(response, function (e, r) {
+                        jQuery_1_12_4.each(response, function (e, r) {
                        var tile = '    <div class="container">        <section class="cms-boxes">            <div class="container-fluid">                <div class="row">                    <div class="col-md-4 cms-boxes-outer">                        <div class="cms-boxes-items cms-features">                            <div class="boxes-align">                                <div class="small-box">                                    <i class="fa fa-4x fa-laptop">&nbsp;</i>                                    <h2>' + e + '</h2>                                    <p>'   + JSON.stringify(r) +   '</p>                                </div>                            </div>                        </div>                    </div>                </div>            </div>        </section>    </div>'
-            $(tile).appendTo("#viewPanelTile");
+            jQuery_1_12_4(tile).appendTo("#viewPanelTile");
                    })}
 
                 });
